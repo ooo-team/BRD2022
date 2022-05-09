1b23fc646bb89fd312fd1f75275fe03b2d175843
Ilya Savitsky <ipsavitsky234@gmail.com>
2022-05-10 02:28:42 +0300

    new bot schema; bot code

--- robot-code/robot.ino
+++ robot-code/robot.ino
@@ -1,328 +1,220 @@
-#include "Adafruit-PWM-Servo-Driver-Library/Adafruit_PWMServoDriver.h"
-#include "gprs-shield/GPRS_Shield_Arduino.h"
+#define TINY_GSM_MODEM_SIM800
+#include <SoftwareSerial.h>
+SoftwareSerial SerialAT(12, 13);
+#if !defined(TINY_GSM_RX_BUFFER)
+#define TINY_GSM_RX_BUFFER 650
+#endif
+#define GSM_AUTOBAUD_MIN 9600
+#define GSM_AUTOBAUD_MAX 115200
+#define GSM_PIN ""
+#include <Adafruit_PWMServoDriver.h>
+#include <ArduinoHttpClient.h>
+#include <TinyGsmClient.h>
+#include <Wire.h>
 
-int Left_motor_go = 8;       //AIN2 connects to port 8 of Arduino UNO for control Left motor forward 
-int Left_motor_back = 7;     //AIN2 connects to port 7 of Arduino UNO for control Left motor back
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+TinyGsm modem(SerialAT);
+TinyGsmClient client(modem);
 
-int Right_motor_go = 2;      //BIN1 connects to port 2 of Arduino UNO for control Right motor forward 
-int Right_motor_back = 4;    //BIN2 connects to port 4 of Arduino UNO for control Right motor back
+const char apn[] = "my apn";
+const char gprsUser[] = "";
+const char gprsPass[] = "";
 
-int Left_motor_pwm = 6;      //PWMA connects to port 6 of Arduino UNO for control the speed of the left motor
-int Right_motor_pwm = 5;     //PWMA connects to port 5 of Arduino UNO for control the speed of the right motor
+const char server[] = "server";
+const uint16_t port = 123;
+HttpClient http(client, server, port);
 
-int key = A0;                //Key connects to wiringPi port 10 of Arduino UNO 
+enum class FirstDriver {
+  Left_go = 4,
+  Left_back = 5,
+  Right_go = 6,
+  Right_back = 7,
+  Left_PWM = 5,
+  Right_PWM = 3
+};
 
-//TrackSensorLeftPin1 TrackSensorLeftPin2 TrackSensorRightPin1 TrackSensorRightPin2
-//      A2                  A1                  A3                   A4
-const int TrackSensorLeftPin1  =  A2;  //The first tracking infrared sensor pin on the left is connected to  wiringPi port A2 of Arduino UNO
-const int TrackSensorLeftPin2  =  A1;  //The second tracking infrared sensor pin on the left is connected to  wiringPi port A1 of Arduino UNO
-const int TrackSensorRightPin1 =  A3;  //The first tracking infrared sensor pin on the right is connected to  wiringPi port A3 of Arduino UNO
-const int TrackSensorRightPin2 =  A4;  //The second tracking infrared sensor pin on the right is connected to  wiringPi port A4 of Arduino UNO
+enum class SecondDriver {
+  Left_go = 8,
+  Left_back = 9,
+  Right_go = 10,
+  Right_back = 11,
+  Left_PWM = 9,
+  Right_PWM = 6
+};
 
-//Define variables to store data collected by each tracking infrared pin
-int TrackSensorLeftValue1;
-int TrackSensorLeftValue2;
-int TrackSensorRightValue1;
-int TrackSensorRightValue2;
+enum class ThirdDriver {
+  Left_go = 12,
+  Left_back = 13,
+  Right_go = 14,
+  Right_back = 15,
+  Left_PWM = 11,
+  Right_PWM = 10
+};
 
-/**
-* Function       setup
-* @author        Danny
-* @date          2017.07.25
-* @brief         Initialization configure
-* @param[in]     void
-* @retval        void
-* @par History   
-*/
-void setup()
-{
-  //Initialize the motor drive IO as the output mode
-  pinMode(Left_motor_go, OUTPUT);
-  pinMode(Left_motor_back, OUTPUT);
-  pinMode(Right_motor_go, OUTPUT);
-  pinMode(Right_motor_back, OUTPUT);
+enum class IRArray {
+  Left_sensor = 2,
+  Left_center_sensor = 4,
+  Rigth_center_sensor = 7,
+  Right_sensor = 8
+};
 
-  //Initialize the key interface as the input mode
-  pinMode(key, INPUT);
+#define DC_FORWARD(driver, speed)                                              \
+  pwm.setPWM(int(driver::Left_go), 4096, 0);                                   \
+  pwm.setPWM(int(driver::Right_go), 4096, 0);                                  \
+  pwm.setPWM(int(driver::Left_back), 0, 4096);                                 \
+  pwm.setPWM(int(driver::Right_back), 0, 4096);                                \
+  digitalWrite(int(driver::Right_PWM), speed);                                 \
+  digitalWrite(int(driver::Left_PWM), speed);
 
-  //Initialize the infrared sensor interface as the input mode
-  pinMode(TrackSensorLeftPin1, INPUT);
-  pinMode(TrackSensorLeftPin2, INPUT);
-  pinMode(TrackSensorRightPin1, INPUT);
-  pinMode(TrackSensorRightPin2, INPUT);
+#define DC_BACK(driver, speed)                                                 \
+  pwm.setPWM(int(driver::Left_go), 0, 4096);                                   \
+  pwm.setPWM(int(driver::Right_go), 0, 4096);                                  \
+  pwm.setPWM(int(driver::Left_back), 4096, 0);                                 \
+  pwm.setPWM(int(driver::Right_back), 4096, 0);                                \
+  digitalWrite(int(driver::Right_PWM), speed);                                 \
+  digitalWrite(int(driver::Left_PWM), speed);
 
-  //Initialize the key interface is high level
-  digitalWrite(key, HIGH);
+#define DC_STOP(driver)                                                        \
+  pwm.setPWM(int(driver::Left_go), 0, 4096);                                   \
+  pwm.setPWM(int(driver::Right_go), 0, 4096);                                  \
+  pwm.setPWM(int(driver::Left_back), 0, 4096);                                 \
+  pwm.setPWM(int(driver::Right_back), 0, 4096);                                \
+  digitalWrite(int(driver::Right_PWM), 0);                                     \
+  digitalWrite(int(driver::Left_PWM), 0);
 
-  //Initialize the infrared sensor interface is high level
-  digitalWrite(TrackSensorLeftPin1, HIGH);
-  digitalWrite(TrackSensorLeftPin2, HIGH);
-  digitalWrite(TrackSensorRightPin1, HIGH);
-  digitalWrite(TrackSensorRightPin2, HIGH);
-  
-  key_scan();
+#define DC_LEFT(driver, speed)                                                 \
+  pwm.setPWM(int(driver::Left_go), 0, 4096);                                   \
+  pwm.setPWM(int(driver::Right_go), 4096, 0);                                  \
+  pwm.setPWM(int(driver::Left_back), 4096, 0);                                 \
+  pwm.setPWM(int(driver::Right_back), 0, 4096);                                \
+  digitalWrite(int(driver::Right_PWM), speed);                                 \
+  digitalWrite(int(driver::Left_PWM), speed);
+
+#define DC_RIGHT(driver, speed)                                                \
+  pwm.setPWM(int(driver::Left_go), 4096, 0);                                   \
+  pwm.setPWM(int(driver::Right_go), 0, 4096);                                  \
+  pwm.setPWM(int(driver::Left_back), 0, 4096);                                 \
+  pwm.setPWM(int(driver::Right_back), 4096, 0);                                \
+  digitalWrite(int(driver::Right_PWM), speed);                                 \
+  digitalWrite(int(driver::Left_PWM), speed);
+
+void setup() {
+  Serial.begin(9600);
+
+  TinyGsmAutoBaud(SerialAT, GSM_AUTOBAUD_MIN, GSM_AUTOBAUD_MAX);
+
+  modem.restart();
+  if (GSM_PIN && modem.getSimStatus() != SIM_ANTITHEFT_LOCKED) {
+    modem.simUnlock(GSM_PIN);
+  }
+
+  // pwm shield but not really pwm
+  pwm.begin();
+  pwm.setPWMFreq(1000);
+
+  pinMode(int(FirstDriver::Left_PWM), OUTPUT);
+  pinMode(int(FirstDriver::Right_PWM), OUTPUT);
+
+  pinMode(int(SecondDriver::Left_PWM), OUTPUT);
+  pinMode(int(SecondDriver::Right_PWM), OUTPUT);
+
+  pinMode(int(ThirdDriver::Left_PWM), OUTPUT);
+  pinMode(int(ThirdDriver::Right_PWM), OUTPUT);
+
+  pinMode(int(IRArray::Left_sensor), INPUT);
+  pinMode(int(IRArray::Left_center_sensor), INPUT);
+  pinMode(int(IRArray::Right_sensor), INPUT);
+  pinMode(int(IRArray::Rigth_center_sensor), INPUT);
+
+  Wire.setClock(400000);
 }
 
-/**
-* Function       run
-* @author        Danny
-* @date          2017.07.26
-* @brief         advance
-* @param[in1]    left_speed
-* @param[in2]    right_speed
-* @param[out]    void
-* @retval        void
-* @par History   
-*/
-void run(int left_speed, int right_speed)
-{
-  //Left motor advance
-  digitalWrite(Left_motor_go, HIGH);   
-  digitalWrite(Left_motor_back, LOW); 
-  analogWrite(Left_motor_pwm, left_speed );
-
-  //Right motor advance
-  digitalWrite(Right_motor_go, HIGH); 
-  digitalWrite(Right_motor_back, LOW); 
-  analogWrite(Right_motor_pwm, right_speed);
+void run(uint8_t speed) {
+  DC_FORWARD(FirstDriver, speed)
+  DC_FORWARD(SecondDriver, speed)
+  DC_FORWARD(ThirdDriver, speed)
 }
 
-/**
-* Function       brake
-* @author        Danny
-* @date          2017.07.25
-* @brief         brake
-* @param[in]     time
-* @param[out]    void
-* @retval        void
-* @par History   
-*/
-void brake(int time)
-{
-  digitalWrite(Left_motor_go, LOW);
-  digitalWrite(Left_motor_back, LOW);
-  digitalWrite(Right_motor_go, LOW);
-  digitalWrite(Right_motor_back, LOW);
-
-  delay(time * 100);
+void back(uint8_t speed) {
+  DC_BACK(FirstDriver, speed)
+  DC_BACK(SecondDriver, speed)
+  DC_BACK(ThirdDriver, speed)
 }
 
-/**
-* Function       left
-* @author        Danny
-* @date          2017.07.25
-* @brief         turn left (left aheel stop,right wheel advance)
-* @param[in1]    left_speed
-* @param[in2]    right_speed
-* @param[out]    void
-* @retval        void
-* @par History   
-*/
-void left(int left_speed, int right_speed)
-{
-  //Left motor stop
-  digitalWrite(Left_motor_go, LOW);    
-  digitalWrite(Left_motor_back, LOW); 
-  analogWrite(Left_motor_pwm, left_speed);
-
-  //Right motor advance
-  digitalWrite(Right_motor_go, HIGH);  
-  digitalWrite(Right_motor_back, LOW); 
-  analogWrite(Right_motor_pwm, right_speed);
+void brake() {
+  DC_STOP(FirstDriver)
+  DC_STOP(SecondDriver)
+  DC_STOP(ThirdDriver)
 }
 
-/**
-* Function       right
-* @author        Danny
-* @date          2017.07.25
-* @brief         turn right(right wheel stop,left wheel advance)
-* @param[in1]    left_speed
-* @param[in2]    right_speed
-* @param[out]    void
-* @retval        void
-* @par History   
-*/
-void right(int left_speed, int right_speed)
-{
-  //Left motor advance
-  digitalWrite(Left_motor_go, HIGH);   
-  digitalWrite(Left_motor_back, LOW); 
-  analogWrite(Left_motor_pwm, left_speed);
-
-  //Roght motor stop
-  digitalWrite(Right_motor_go, LOW);  
-  digitalWrite(Right_motor_back, LOW); 
-  analogWrite(Right_motor_pwm, right_speed);
+void left(uint8_t speed) {
+  DC_LEFT(FirstDriver, speed)
+  DC_LEFT(SecondDriver, speed)
+  DC_LEFT(ThirdDriver, speed)
 }
 
-/**
-* Function       spin_left
-* @author        Danny
-* @date          2017.07.25
-* @brief         turn left in place(left wheel back,right wheel advance)
-* @param[in1]    left_speed
-* @param[in2]    right_speed
-* @param[out]    void
-* @retval        void
-* @par History   
-*/
-void spin_left(int left_speed, int right_speed)
-{
-  //Left motor back
-  digitalWrite(Left_motor_go, LOW);     
-  digitalWrite(Left_motor_back, HIGH);  
-  analogWrite(Left_motor_pwm, left_speed);
-
-  //Right motor advance
-  digitalWrite(Right_motor_go, HIGH);
-  digitalWrite(Right_motor_back, LOW); 
-  analogWrite(Right_motor_pwm, right_speed);
+void right(uint8_t speed) {
+  DC_RIGHT(FirstDriver, speed)
+  DC_RIGHT(SecondDriver, speed)
+  DC_RIGHT(ThirdDriver, speed)
 }
 
-/**
-* Function       spin_right
-* @author        Danny
-* @date          2017.07.25
-* @brief          turn right in place(left wheel advance,right wheel back)
-* @param[in1]    left_speed
-* @param[in2]    right_speed
-* @param[out]    void
-* @retval        void
-* @par History   
-*/
-void spin_right(int left_speed, int right_speed)
-{
-  //Left motor advance
-  digitalWrite(Left_motor_go, HIGH);    
-  digitalWrite(Left_motor_back, LOW);   
-  analogWrite(Left_motor_pwm, left_speed);
+void loop() {
+  // if all four are bad - go back
+  if (digitalRead(int(IRArray::Left_sensor)) &
+      digitalRead(int(IRArray::Left_center_sensor)) &
+      digitalRead(int(IRArray::Right_sensor)) &
+      digitalRead(int(IRArray::Rigth_center_sensor)) == HIGH) {
+    back(255);
+    delay(3000);
+    brake();
+  }
+  // GPRS connection parameters are usually set after network registration
+  Serial.print(F("Connecting to "));
+  Serial.print(apn);
+  if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
+    Serial.println(" fail");
+    delay(10000);
+    return;
+  }
+  Serial.println(" success");
 
-  //Right motor back
-  digitalWrite(Right_motor_go, LOW);    
-  digitalWrite(Right_motor_back, HIGH); 
-  analogWrite(Right_motor_pwm, right_speed);
-}
+  if (modem.isGprsConnected()) {
+    Serial.println("GPRS connected");
+  }
+  Serial.print(F("Performing HTTP GET request... "));
+  int err = http.get("/get_next");
+  if (err != 0) {
+    Serial.println(F("failed to connect"));
+    delay(10000);
+    return;
+  }
 
-/**
-* Function       back
-* @author        Danny
-* @date          2017.07.25
-* @brief         back
-* @param[in]     time
-* @param[out]    void
-* @retval        void
-* @par History   
-*/
-void back(int time)
-{
-  //Left motor back
-  digitalWrite(Left_motor_go, LOW);     
-  digitalWrite(Left_motor_back, HIGH); 
-  analogWrite(Left_motor_pwm, 40);
+  int status = http.responseStatusCode();
+  Serial.print(F("Response status code: "));
+  Serial.println(status);
+  if (!status) {
+    delay(10000);
+    return;
+  }
 
-  //Right motor back
-  digitalWrite(Right_motor_go, LOW);    
-  digitalWrite(Right_motor_back, HIGH);
-  analogWrite(Right_motor_pwm, 40);
-
-  delay(time );
-}
-
-/**
-* Function       key_scan
-* @author        Danny
-* @date          2017.07.25
-* @brief         key detection (including software-key debounce)
-* @param[in]     void
-* @param[out]    void
-* @retval        void
-* @par History   
-*/
-void key_scan()
-{
-  while (digitalRead(key));       //Loops this code when the key is not pressed
-  while (!digitalRead(key))       //When the key is pressed
-  {
-    delay(10);	                 
-    if (digitalRead(key)  ==  LOW)//Re-determine whether the key was pressed
-    {
-      delay(100);
-      while (!digitalRead(key));  //Determine whether the key is released 
-    }
+  String body = http.responseBody();
+  Serial.println(F("Response:"));
+  switch (body.toInt()) {
+  case 0:
+    run(255);
+    break;
+  case 1:
+    left(255);
+    break;
+  case 2:
+    right(255);
+    break;
+  case 3:
+    back(255);
+    break;
+  case 4:
+    brake();
+    break;
   }
 }
-
-/**
-* Function       loop
-* @author        Danny
-* @date          2017.07.25
-* @brief         
-* @param[in]     void
-* @retval        void
-* @par History   
-*/
-void loop()
-{
-   // When the black line is detected, the corresponding indicator of the tracking module is on, and the port level is LOW.
-   // When the black line is not detected, the corresponding indicator of the tracking module is off, and the port level is HIGH.
-  TrackSensorLeftValue1  = digitalRead(TrackSensorLeftPin1);
-  TrackSensorLeftValue2  = digitalRead(TrackSensorLeftPin2);
-  TrackSensorRightValue1 = digitalRead(TrackSensorRightPin1);
-  TrackSensorRightValue2 = digitalRead(TrackSensorRightPin2);
-
-  //4 tracking pins level status
-  // 0 0 X 0
-  // 1 0 X 0
-  // 0 1 X 0
-  if ( (TrackSensorLeftValue1 == LOW || TrackSensorLeftValue2 == LOW) &&  TrackSensorRightValue2 == LOW)
-  {
-    spin_right(250, 250);
-    delay(80);
-  }
-  //4 tracking pins level status
-  // 0 X 0 0       
-  // 0 X 0 1 
-  // 0 X 1 0       
-  //Turn right in place,speed is 150,delay 80ms   
-  //Handle left acute angle and left right angle 
-  else if ( TrackSensorLeftValue1 == LOW && (TrackSensorRightValue1 == LOW ||  TrackSensorRightValue2 == LOW))
-  {
-    spin_left(250, 250);
-    delay(80);
-  }
-  // 0 X X X
-  //Left_sensor1 detected black line
-  else if ( TrackSensorLeftValue1 == LOW)
-  {
-    spin_left(150, 150);
-   //delay(2);
-  }
-  // X X X 0
-  //Right_sensor2 detected black line
-  else if ( TrackSensorRightValue2 == LOW )
-  {
-    spin_right(150, 150);
-    //delay(2);
-  }
-  //4 tracking pins level status
-  // X 0 1 X
-  else if ( TrackSensorLeftValue2 == LOW && TrackSensorRightValue1 == HIGH)
-  {
-    left(0, 220);
-  }
-  //4 tracking pins level status
-  // X 1 0 X  
-  else if (TrackSensorLeftValue2 == HIGH && TrackSensorRightValue1 == LOW)
-  {
-    right(220, 0);
-  }
-  //4 tracking pins level status
-  // X 0 0 X
-  else if (TrackSensorLeftValue2 == LOW && TrackSensorRightValue1 == LOW)
-  {
-    run(255, 255);
-  }
-  //When the level of 4 pins are 1 1 1 1 , the car keeps the previous running state.
-}
-
-
