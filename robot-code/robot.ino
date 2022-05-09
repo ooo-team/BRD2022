335e745fcf7cf4f95edb6dbba00aefffcb0d3c14
Ilya Savitsky <ipsavitsky234@gmail.com>
2022-05-09 23:28:59 +0300

    edited routing

--- robot-code/robot.ino
+++ robot-code/robot.ino
@@ -1,4 +1,5 @@
 #include "Adafruit-PWM-Servo-Driver-Library/Adafruit_PWMServoDriver.h"
+#include "gprs-shield/GPRS_Shield_Arduino.h"
 
 int Left_motor_go = 8;       //AIN2 connects to port 8 of Arduino UNO for control Left motor forward 
 int Left_motor_back = 7;     //AIN2 connects to port 7 of Arduino UNO for control Left motor back
